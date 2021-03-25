b118481e53259f57eae7e9836a19aaeda91fb89f
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2021-03-25 09:18:47 +0100

    Odsun nastroju
    fix

--- Mereni.m
+++ Mereni.m
@@ -1,4 +1,5 @@
 addpath('Mereni')
+addpath('fittingTools')
 plotORnot = true;
 
 % Kotva
@@ -8,7 +9,7 @@ out.R_a = U_a/I_a;
 
 PK = table2array(readtable('Mereni/PrechodKotva.CSV','range',[19,4]));
 tau_K = fitFOSresponse(0,[],PK(:,1),PK(:,2),plotORnot);
-out.L_a = tau_K*R_a;
+out.L_a = tau_K*out.R_a;
 
 % Buzeni
 I_b = 0.5; %A
@@ -17,7 +18,7 @@ out.R_b = U_b/I_b;
 
 PB = table2array(readtable('Mereni/PrechodBuzeni.CSV','range',[19,4]));
 tau_B = fitFOSresponse(0,[],PB(:,1),PB(:,2),plotORnot);
-out.L_b = tau_B*R_b;
+out.L_b = tau_B*out.R_b;
 
 % Konst. stroje
 U_a = 60.8; %V
@@ -25,7 +26,7 @@ n = 875; %min^-1
 I_a = 2.6; %A
 I_b = 1.5; %A
 
-U_i = U_a - R_a*I_a;
+U_i = U_a - out.R_a*I_a;
 om = n*pi/30;
 out.C_ss = U_i / (om*I_b);
 
