5b74558861abd4d1c983fe572aa78e70e774d079
kopecvo2 <81413409+kopecvo2@users.noreply.github.com>
2021-03-27 12:48:12 +0100

    Add files via upload

--- SimRun.m
+++ SimRun.m
@@ -1,4 +1,4 @@
-function SimOut = SimRun(meas,t_0,U_id,delta)
+function SimOut = SimRun(meas,t_0,U_a,U_b,delta)
 	%meas = load('MereneHodnoty.mat');
 
 	R_a = meas.R_a + delta(1);
@@ -8,8 +8,8 @@ function SimOut = SimRun(meas,t_0,U_id,delta)
 	c_ss = meas.c_ss + delta(5);
 	M_z = meas.M_z + delta(6);
 	J = meas.J + delta(7);
-	U_a = U_id + delta(8);
-	U_b = U_id + delta(9);
+	U_a = U_a + delta(8);
+	U_b = U_b + delta(9);
 
 	options = simset('SrcWorkspace','current');
 	SimOut = sim('DCmotor',[],options);
